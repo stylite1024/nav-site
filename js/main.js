8b13c884e37144db3f94a8e072ae957393760327
ZenZone <37891333+stylite1024@users.noreply.github.com>
2025-02-04 22:07:45 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -76,21 +76,6 @@ function rgb2hex(rgb) {
 $(function() {
         var bookmark = {
             data: [{
-                name: '开发日志',
-                link: 'https://kksan.top/f5404b68deeb4634b39dac0bc67ec693',
-                box_shadow: '#F18033',
-                icon: 'img/log.png',
-            }, {
-                name: '酷安',
-                link: 'https://www.coolapk.com',
-                box_shadow: '#11B063',
-                icon: 'img/coolapk.png',
-            }, {
-                name: 'Via插件',
-                link: 'http://via-app.cn',
-                box_shadow: '#FA7199',
-                icon: 'img/via.png',
-            }, {
                 name: '今日热榜',
                 link: 'https://tophub.today/',
                 box_shadow: '#F2584A',
@@ -105,11 +90,6 @@ $(function() {
                 link: 'https://m.mi.com/',
                 box_shadow: '#F57923',
                 icon: 'img/mi.png',
-            }, {
-                name: 'unsplash',
-                link: 'https://unsplash.com/',
-                box_shadow: '#2C2C2C',
-                icon: 'img/unsplash.png',
             }, {
                 name: '豆瓣',
                 link: 'https://m.douban.com/',
@@ -130,26 +110,6 @@ $(function() {
                 link: 'https://www.csdn.net/',
                 box_shadow: '#c5000a',
                 icon: 'img/csdn.png',
-            }, {
-                name: 'XDA',
-                link: 'https://www.xda-developers.com/',
-                box_shadow: '#AC6E2F',
-                icon: 'img/xda.png',
-            }, {
-                name: '少数派',
-                link: 'https://sspai.com/',
-                box_shadow: '#D7191A',
-                icon: 'img/sspai.png',
-            }, {
-                name: '小众软件',
-                link: 'https://www.appinn.com/',
-                box_shadow: '#3279ea',
-                icon: 'img/xiaozhong.png',
-            }, {
-                name: '数字尾巴',
-                link: 'https://www.dgtle.com/',
-                box_shadow: '#62677b',
-                icon: 'img/shuziweiba.png',
             }]
         }
         for (var i = 0; i < bookmark.data.length; i++) {
